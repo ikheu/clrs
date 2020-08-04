10dfac8cc59c7b2398fbf5a30c43cb1324472446
ikheu <iceking608@gmail.com>
2020-08-04 23:00:29 +0800

    heap

--- heap.py
+++ heap.py
@@ -1,21 +1,65 @@
 class Heap:
     def __init__(self, data):
-        self._data = [None]
-        self._data.extend(data)
+        # self._data = [None]
+        self._data = data
+        self.build()
 
-    def node_parent(self, index):
-        return self._data(index >> 1)
+    def parent(self, i):
+        return ((i+1) >> 1) - 1
 
-    def node_left(self, index):
-        index = index << 1
-        if index >= len(self._data):
-            return self._data(index)
+    def left(self, i):
+        return 2*i + 1
+
+    def right(self, i):
+        return 2*i + 2
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, i):
+        return self._data[i]
+
+    def __setitem__(self, i, v):
+        self._data[i] = v
+
+    def max_heapify(self, i, size):
+        l, r = self.left(i), self.right(i)
+        if l < size and self[l] > self[i]:
+            lagest = l
         else:
-            return None
+            lagest = i
+        if r < size and self[r] > self[lagest]:
+            lagest = r
+        if lagest != i:
+            self[lagest], self[i] = self[i], self[lagest]
+            self.max_heapify(lagest, size)
 
-    def node_right(self, index):
-        index = (index << 1) + 1
-        if index >= len(self._data):
-            return self._data(index)
-        else:
-            return None
+    def build(self):
+        for i in range(len(self) >> 1, -1, -1):
+            self.max_heapify(i, len(self))
+
+    def sort(self):
+        size = len(self)
+        for i in range(len(self) - 1, 0, -1):
+            self[0], self[i] = self[i], self[0]
+            size -= 1
+            self.max_heapify(0, size)
+
+    def __repr__(self):
+        return "heap(%s)" % self._data
+
+if __name__ == '__main__':
+    arr = [8, 7, 9, 16, 4, 10, 3, 2, 1, 14]
+    h = Heap(arr)
+    print(h)
+    h.sort()
+    print(h)
+
+# i l r p
+# 1 2 3 0
+# 2 4 5 1
+# 3 6 7 1
+
+# 0 1 3 -1  (i+1) * 2 - 1 = 2*i+1, 2*i+2, (i+1)//2-1
+# 1 3 4 0
+# 2 5 6 0
